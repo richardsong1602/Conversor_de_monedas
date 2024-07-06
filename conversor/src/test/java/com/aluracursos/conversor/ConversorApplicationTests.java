91bd4ac8efbbf759125b966107f0392bb9c246c7
Ricardo Barragan <richards02ba@gmail.com>
2024-07-06 13:42:44 -0600

    Conversor commit

---
+++ conversor/src/test/java/com/aluracursos/conversor/ConversorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.conversor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
